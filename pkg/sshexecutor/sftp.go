8b74959017b241a664c75794899f8178b0cece4a
Alexander Trost <galexrt@googlemail.com>
2018-07-13 11:17:08 +0200

    Added SSHExecutor with SFTP "integration"

---
+++ pkg/sshexecutor/sftp.go
@@ -0,0 +1,28 @@
+/*
+Copyright (c) 2018 Alexander Trost <galexrt@googlemail.com>. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package sshexecutor
+
+import (
+	"github.com/pkg/sftp"
+)
+
+// SFTP create new SFTP client using github.com/pkg/sftp package
+func (se *SSHExecutor) SFTP() error {
+	var err error
+	se.sftpClient, err = sftp.NewClient(se.client)
+	return err
+}
